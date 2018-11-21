158c2ce459177dddc4b7f2b0fd3264df9e058657
chidera <dexiouz@gmail.com>
2018-11-21 17:26:23 +0100

    Adding landing page and basic campground details

--- campDera/app.js
+++ campDera/app.js
@@ -1,13 +1,23 @@
 let express = require("express");
 let app = express();
 
+app.use( express.static( "public"));
 app.set("view engine", "ejs");
 
 app.get("/", ( req, res)=>{
   res.render( "landing" )
 })
 
+app.get( '/campgrounds', ( req, res )=> {
+  let campgrounds = [
+    {name: "Eastern camp", image: '/assets/mbuntu-1.jpg'},
+    {name: "Western camp", image: '/assets/mbuntu-2.jpg'},
+    {name: "Northern camp", image: '/assets/mbuntu-3.jpg'},
+    {name: "Southern camp", image: '/assets/mbuntu-4.jpg'}
+  ];
 
+  res.render( "campgrounds", { campgrounds })
+})
 
 app.listen( 3000, ()=>{
   console.log( "The camp server has started" )
