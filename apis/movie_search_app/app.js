fb3ece2f98b807fbc31fcfcb7f5e1205fd1a00e1
chidera <dexiouz@gmail.com>
2018-11-21 14:22:41 +0100

    started the movies app

---
+++ apis/movie_search_app/app.js
@@ -0,0 +1,24 @@
+let express = require( "express" );
+let app = express();
+let request = require( "request" );
+
+app.set( "view engine", "ejs")
+
+app.get('/', (req, res)=>{
+  res.render("search")
+})
+app.get( '/results', (req, res) => {
+  request( "http://www.omdbapi.com/?s=california&apikey=b052c23", ( error, response, body ) => {
+    if( !error && response.statusCode == 200) {
+      let data = JSON.parse( body ); 
+      res.render( "results", { data }); 
+    }
+  })
+});
+
+
+
+
+app.listen( 3000, ()=> {
+  console.log("movie server started")
+})
