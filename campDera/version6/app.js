ea0b9fd61d0f52f91a27d7a328cb193f95bf2d02
chidera <dexiouz@gmail.com>
2018-12-31 09:30:24 +0100

    Completed login logic for version camp app

--- campDera/version6/app.js
+++ campDera/version6/app.js
@@ -141,9 +141,16 @@ app.post("/register", (req,res)=>{
 
 //show login form
 app.get("/login", (req,res)=>{
-  res.render("/login")
+  res.render("login")
 });
 
+//handling login logic
+app.post("/login",passport.authenticate("local", 
+  {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+  }), (req,res)=>{
+});
 
 app.listen( 3000, ()=>{
   console.log( "The camp server has started" )
