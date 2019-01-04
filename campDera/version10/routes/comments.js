f445b34e777724882e138847d9ae5de0de59c8ca
chidera <dexiouz@gmail.com>
2019-01-04 09:16:58 +0100

    Can delete comment without authorisation

--- campDera/version10/routes/comments.js
+++ campDera/version10/routes/comments.js
@@ -44,7 +44,7 @@ router.post("/",isLoggedIn, ( req, res) => {
 router.get("/:comment_id/edit", (req, res) => {
     Comment.findById(req.params.comment_id, (err, foundComment) => {
       if(err){
-        console.log(err)
+        res.redirect("back")
       } else {
         res.render("comments/edit", {campground_id: req.params.id, comment: foundComment})
       }
@@ -55,13 +55,23 @@ router.get("/:comment_id/edit", (req, res) => {
 router.put("/:comment_id", (req, res) => {
   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
     if(err){
-      console.log(err)
+      res.redirect("back")
     } else{
       res.redirect(`/campgrounds/${req.params.id}`)
     }
   })
-} )
+});
 
+//Comment delete route
+router.delete("/:comment_id", (req,res) => {
+  Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+    if(err){
+      res.redirect("back")
+    } else {
+      res.redirect(`/campgrounds/${req.params.id}`)
+    }
+  })
+})
 // isLoggedIn function
 function isLoggedIn(req,res,next){
   if(req.isAuthenticated()){
